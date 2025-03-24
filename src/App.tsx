11c962e96c792587f15a040658c3d442b1c23af5
dvErick <48191311+athusderia@users.noreply.github.com>
2025-03-24 17:53:31 -0600

    primer commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import MenuItems from "./components/MenuItems";
+import { menuItems } from "./data/db";
+
+function App() {
+  return (
+    <>
+      <header className=" bg-teal-400 py-5">
+        <h1 className=" text-center text-4xl font-black">
+          Claucladora de Propinas y Consumo
+        </h1>
+      </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2 ">
+        <div className=" p-5">
+          <h2 className=" text-4xl font-black">Menu</h2>
+          <div className=" space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItems key={item.id} item={item} />
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
